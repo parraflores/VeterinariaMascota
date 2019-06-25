f5a05fe0bedf6cc7cec15ec71a28f38838b2626f
parraflores <lusmeryparra25@gmail.com>
2019-06-25 12:42:47 -0500

    subiendo mi trabajao

---
+++ src/java/managedBean/MascotaBean.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managedBean;
+
+import dao.MascotaDao;
+import entidades.Mascota;
+import java.io.Serializable;
+import java.util.ArrayList;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import org.hibernate.HibernateException;
+
+/**
+ *
+ * @author LUSMERY
+ */
+@ManagedBean
+//@RequestScoped
+@ViewScoped
+public class MascotaBean implements Serializable {
+
+    private Mascota mascota;
+
+    public MascotaBean() {
+        this.mascota = new Mascota();
+
+    }
+
+    public Mascota getMascota() {
+        return mascota;
+    }
+
+    public void setMascota(Mascota mascota) {
+        this.mascota = mascota;
+    }
+
+    public String guardarMascota() {
+        try {
+
+            MascotaDao mascotaDao = new MascotaDao();
+            boolean respuesta = mascotaDao.guardarMascota(mascota);
+            if (respuesta) {
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Se registro con exito"));
+            } else {
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("No se puedo registrar"));
+            }
+        } catch (HibernateException e) {
+            ///transation.rollback();  -- regresa a la anterior
+            System.out.println("Error::: " + e);
+        }
+        return "/RegistroMascota";
+    }
+
+    public String actualizarMascota() {
+        try {
+            MascotaDao mascotaDao = new MascotaDao();
+            boolean respuesta = mascotaDao.actualizarMascota(mascota);
+            if (respuesta){
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Se actualizo con exito"));
+            } else {
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("No se puedo actualizar"));
+            }
+        } catch (HibernateException e) {
+            ///transation.rollback();  -- regresa a la anterior
+            System.out.println("Error::: " + e);
+        }
+        return "/RegistroMascota";
+
+    }
+
+    public ArrayList<Mascota> listarMascotas() {
+        ArrayList<Mascota> lista = new ArrayList<>();
+        MascotaDao mascotaDao = new MascotaDao();
+        lista = mascotaDao.listarMascotas();
+        return lista;
+    }
+
+    public String eliminar() {
+        MascotaDao mascotaDao = new MascotaDao();
+        boolean respuesta = mascotaDao.eliminarMascota(mascota);
+        if (respuesta) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Se elimino con exito"));
+        } else {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("No se puedo eliminar"));
+        }
+        return "/RegistroMascota";
+    }
+
+    public String limpiar() {
+        return "/RegistroMascota";
+    }
+
+}
